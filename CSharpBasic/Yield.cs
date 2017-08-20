1768678066d6e46062552215fa8be4a64f078157
iyeongjun <they6687@naver.com>
2017-08-20 16:30:05 +0900

    Yield

--- CSharpBasic/Yield.cs
+++ CSharpBasic/Yield.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Messaging;
@@ -10,12 +11,13 @@ namespace CSharpBasic
         static void Main(string[] args)
         {
              YieldBasic yieldBasic = new YieldBasic();
+            yieldBasic.YieldInteractionForEach();
         }
     }
 
     public class YieldBasic
     {
-        static IEnumerable<string> yieldString()
+        static IEnumerable<string> YieldString()
         {
             yield return "first";     //첫번째 루프에서 리턴됨
             yield return "second";    //두번째 루프에서 리턴됨
@@ -25,7 +27,7 @@ namespace CSharpBasic
 
         private int[] data = {1, 2, 3, 4, 5}; //데이터 배열 정의
 
-        public IEnumerator GetEnumerator() // 
+        public IEnumerator GetEnumerator() //  yield를 사용하기 위해 IEnumerator 인터페이스 구현
         {
             int i = 0;
             while (i < data.Length)
@@ -35,10 +37,23 @@ namespace CSharpBasic
             }
         }
 
-        public void InteractionForeach()
+        public void YieldInteractionForEach()  //루프를 이용한 인터랙션
         {
             var yieldBasic = new YieldBasic();
-            
+            foreach (var item in yieldBasic)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void YieldInteractionMoveNext() //수동을 이용한 인터랙션
+        {
+            var yieldBasic = new YieldBasic();
+            IEnumerator it = yieldBasic.GetEnumerator();
+            it.MoveNext();
+            Console.WriteLine(it.Current);
+            it.MoveNext();
+            Console.WriteLine(it.Current);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # CSharpBasic
+
+## yield
+
+yield 는 컬렉션 데이터를 하나씩 리턴할 떄 사용함.
+
+```c#
+static IEnumerable<string> YieldString()
+        {
+            yield return "first";     //첫번째 루프에서 리턴됨
+            yield return "second";    //두번째 루프에서 리턴됨
+            yield return "thrid";     //세번쨰 루프에서 리턴됨
+            yield break;              //Yield break문
+        }
+```
+
+## Enumerator 인터페이스
+
+yield를 사용하기 위해선 IEnumerator 인터페이스를 구현해야하며, IEnumerator 인터페이스는 
+<li> Current : 현재 값
+<li> MoveNext() : 다음 yield field로 이동
+<li> Reset() : 리셋
+ 이렇게 세가지가 존재하며 Enumerator가 되기위해선 Current와 MoveNext()를 반드시 구현해야함
+ 
+ ## 출력
+
+```c#
+private int[] data = {1, 2, 3, 4, 5}; //데이터 배열 정의
+
+        public IEnumerator GetEnumerator() //  yield를 사용하기 위해 IEnumerator 인터페이스 구현
+        {
+            int i = 0;
+            while (i < data.Length)
+            {
+                yield return data[i];
+                i++;
+            }
+        }
+
+        public void YieldInteractionForEach()  //루프를 이용한 인터랙션
+        {
+            var yieldBasic = new YieldBasic();
+            foreach (var item in yieldBasic)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void YieldInteractionMoveNext() //수동을 이용한 인터랙션
+        {
+            var yieldBasic = new YieldBasic();
+            IEnumerator it = yieldBasic.GetEnumerator();
+            it.MoveNext();
+            Console.WriteLine(it.Current);
+            it.MoveNext();
+            Console.WriteLine(it.Current);
+        }
+```
